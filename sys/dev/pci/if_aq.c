61becac8f37f505fb37ad16061816e310133c365
Ryo Shimizu <ryo@nerv.org>
2020-01-01 02:06:47 +0900

    2019 -> 2020

--- sys/dev/pci/if_aq.c
+++ sys/dev/pci/if_aq.c
@@ -36,7 +36,7 @@
  */
 
 /*-
- * Copyright (c) 2019 Ryo Shimizu <ryo@nerv.org>
+ * Copyright (c) 2020 Ryo Shimizu <ryo@nerv.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
